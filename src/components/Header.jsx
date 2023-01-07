34c2f5766850c30dde67c2369d0c7e95f57705e0
Andrea <andrea.dauria@fanplayr.com>
2023-01-07 16:05:13 +0100

    Add: StickyNavbar functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,40 @@
 import ReactDOM from 'react-dom'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import NavBarTop from './NavBarTop'
 import NavBar from './NavBar'
+import heroImg from '/src/assets/image/heroImage.jpg'
 import '../assets/styles/Header.scss'
 
 const Header = () => {
+    const [stickyClass, setStickyClass] = useState("navBarContainer");
+    const [headSticky, setheadSticky] = useState("navBarContainer");
+
+    useEffect(() => {
+        window.addEventListener('scroll', stickNavbar);
+        return () => {
+            window.removeEventListener('scroll', stickNavbar);
+        };
+    }, []);
+
+    const stickNavbar = () => {
+        if (window.scrollY > 0) {
+            setStickyClass("navBarContainerActive");
+            setheadSticky("");
+        } else {
+            setStickyClass("navBarContainer");
+            setheadSticky("fixed");
+        }
+    };
 
     return (
-        <header>
-            <NavBarTop />
-            <div className="headerHero">
-                <h1 className="headerHeroTitle">Lumens</h1>
+        <header className={headSticky} >
+            <div className={stickyClass} >
+                <NavBarTop />
+                <div className="headerHero">
+                    <h1 className="headerHeroTitle">Lumens</h1>
+                </div>
+                <NavBar />
             </div>
-            <NavBar />
         </header>
     );
 }
